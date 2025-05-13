7dc189cee24ac54f5318a0de7b0d310feea7a7a8
Ryan Harmon <ryan@bloomcu.com>
2025-05-12 18:47:32 -0600

    Fix api route file

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WebsiteController;
 
-Route::resource('websites', WebsiteController::class);
+Route::get('/', function () {
+    return 'Hello world';
+});
