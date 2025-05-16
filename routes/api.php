5631138bcda9eb6cbf04c3c675f2504144321ae3
Ryan Harmon <ryan@bloomcu.com>
2025-05-16 16:06:39 -0600

    feat: implement website pages with CRUD endpoints and relationships

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WebsiteController;
 use App\Http\Controllers\ConversationController;
 use App\Http\Controllers\ChatController;
+use App\Http\Controllers\PageController;
 
 Route::resource('conversations', ConversationController::class);
 Route::resource('conversations/{conversation}/chats', ChatController::class);
 Route::resource('websites', WebsiteController::class);
+Route::resource('websites/{website}/pages', PageController::class);
