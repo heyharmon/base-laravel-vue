cffb1c424354a6d8fa980ed3ea6e4c535771be78
Ryan Harmon <ryan@bloomcu.com>
2025-05-17 17:46:57 -0600

    style: change default layout background from neutral to white

--- resources/js/layouts/DefaultLayout.vue
+++ resources/js/layouts/DefaultLayout.vue
@@ -3,7 +3,7 @@ import AppNav from '@/components/AppNav.vue';
 </script>
 
 <template>
-  <div class="min-h-screen bg-neutral-100">
+  <div class="min-h-screen bg-white">
     <AppNav />
     
     <main class="max-w-7xl mx-auto px-4">
