db7d7744715aa71e340b39cb947b978dcadd3985
Ryan Harmon <heyharmon@gmail.com>
2025-05-23 14:43:13 -0600

    Add model factories for testing

---
+++ database/factories/ConversationFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Conversation;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Conversation>
+ */
+class ConversationFactory extends Factory
+{
+    protected $model = Conversation::class;
+
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence(3),
+        ];
+    }
+}
