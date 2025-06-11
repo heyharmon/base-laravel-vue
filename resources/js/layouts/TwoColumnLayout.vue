874d0f15aa4e18118097ca14b1d724583cf03a9b
Ryan Harmon <ryan@bloomcu.com>
2025-06-11 12:26:25 -0600

    feat: implement two-column layout with editor menu component refactor

---
+++ resources/js/layouts/TwoColumnLayout.vue
@@ -0,0 +1,23 @@
+<script setup>
+import AppNav from '@/components/AppNav.vue'
+</script>
+
+<template>
+	<div class="min-h-screen bg-white">
+		<AppNav />
+
+		<main class="mx-auto px-6">
+			<div class="flex flex-row gap-4">
+				<!-- Left column (fixed width) -->
+				<div class="w-1/3">
+					<slot name="left-column" />
+				</div>
+
+				<!-- Right column (flexible width) -->
+				<div class="w-2/3">
+					<slot name="right-column" />
+				</div>
+			</div>
+		</main>
+	</div>
+</template>
