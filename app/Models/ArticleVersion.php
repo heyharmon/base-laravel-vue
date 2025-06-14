7b4c62ae4ffe0527dbfba0ee7b6017cf7b3a71ba
Ryan Harmon <ryan@bloomcu.com>
2025-06-14 14:52:41 -0600

    feat: add pruning for article versions older than 1 month

--- app/Models/ArticleVersion.php
+++ app/Models/ArticleVersion.php
@@ -5,9 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Casts\AsCollection;
+use Illuminate\Database\Eloquent\Prunable;
 
 class ArticleVersion extends Model
 {
+	use Prunable;
+
 	/**
 	 * The attributes that are mass assignable.
 	 *
@@ -27,6 +30,17 @@ class ArticleVersion extends Model
 	protected $casts = [
 		'data' => AsCollection::class,
 	];
+
+	/**
+	 * Get the prunable model query.
+	 *
+	 * @return \Illuminate\Database\Eloquent\Builder
+	 */
+	public function prunable()
+	{
+		// Prune versions older than 1 month
+		return static::where('created_at', '<=', now()->subMonth());
+	}
 
 	/**
 	 * Get the article that owns the version.
--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,11 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
+use Illuminate\Support\Facades\Schedule;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Foundation\Inspiring;
+
+Schedule::command('model:prune')->daily();
 
 Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
+	$this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
