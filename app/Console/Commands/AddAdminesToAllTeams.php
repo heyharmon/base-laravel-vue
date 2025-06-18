b7e084ec1f0570cf98899d98776209b8e4ff530f
Ryan Harmon <ryan@bloomcu.com>
2025-06-18 13:30:10 -0600

    chore: remove Elisha from admin team assignment list

--- app/Console/Commands/AddAdminesToAllTeams.php
+++ app/Console/Commands/AddAdminesToAllTeams.php
@@ -28,7 +28,7 @@ class AddAdminesToAllTeams extends Command
 	 */
 	public function handle()
 	{
-		$userEmails = ['derik.krauss@metrifi.com', 'ryan.harmon@metrifi.com', 'elisha@bloomcu.com'];
+		$userEmails = ['derik.krauss@metrifi.com', 'ryan.harmon@metrifi.com'];
 		$teams = Team::all();
 		$now = Carbon::now();
 		$addedCount = 0;
