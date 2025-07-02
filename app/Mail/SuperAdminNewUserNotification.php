24a93b1acc3623812af4766b6c967c68a1e08ce6
Ryan Harmon <ryan@bloomcu.com>
2025-07-02 11:27:32 -0600

    feat: add super admin email notifications for new user registrations

---
+++ app/Mail/SuperAdminNewUserNotification.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class SuperAdminNewUserNotification extends Mailable
+{
+	use Queueable, SerializesModels;
+
+	/**
+	 * Create a new message instance.
+	 */
+	public function __construct(
+		public User $newUser
+	) {}
+
+	/**
+	 * Get the message envelope.
+	 */
+	public function envelope(): Envelope
+	{
+		return new Envelope(
+			subject: 'New User Registration: ' . $this->newUser->name,
+		);
+	}
+
+	/**
+	 * Get the message content definition.
+	 */
+	public function content(): Content
+	{
+		return new Content(
+			text: 'emails.super-admin-new-user-notification',
+		);
+	}
+
+	/**
+	 * Get the attachments for the message.
+	 *
+	 * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+	 */
+	public function attachments(): array
+	{
+		return [];
+	}
+}
